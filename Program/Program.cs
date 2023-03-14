03e5109c0f816158c688612fd0a74a0884c49dc9
alex-ush <mastak@bk.ru>
2023-03-15 00:54:42 +0300

    readme done

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,12 +1,13 @@
 ﻿Console.WriteLine("Сколько элементов будет в вашем массиве?");
-int size  = int.Parse(Console.ReadLine());
+int size = int.Parse(Console.ReadLine());
 
 string[] arr1 = new string[size];
 
-for (int i = 0; i < size; i++){
-	Console.Write($"Ведите {i+1} элемент массива: ");
+for (int i = 0; i < size; i++)
+{
+    Console.Write($"Ведите {i + 1} элемент массива: ");
     string result = Console.ReadLine();
-	arr1[i] = result;  
+    arr1[i] = result;
 }
 Console.WriteLine();
 Console.Write("Ваш массив: ");
@@ -15,17 +16,21 @@ Console.Write(string.Join(" ", arr1));
 int count = 0;
 int maxLength = 3;
 
-for (int i = 0; i < arr1.Length; i++){
-	if(arr1[i].Length <= maxLength){
-		count++;
-	}   
+for (int i = 0; i < arr1.Length; i++)
+{
+    if (arr1[i].Length <= maxLength)
+    {
+        count++;
+    }
 }
 Console.WriteLine();
 Console.Write("Обработанный массив: ");
 string[] arr2 = new string[count];
 int j = 0;
-for (int i = 0; i < arr1.Length; i++){
-    if(arr1[i].Length <= maxLength){
+for (int i = 0; i < arr1.Length; i++)
+{
+    if (arr1[i].Length <= maxLength)
+    {
         arr2[j] = arr1[i];
         Console.Write(arr2[j] + " ");
         j++;
